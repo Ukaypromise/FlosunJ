86461df6551285f93a34fc5b71ddf269adfeab08
github_Ukapromise <ukapromiseimo@gmail.com>
2022-06-11 21:16:39 +0100

    Added more sections to homepage

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 // Toggle menu
 const navToggle = document.querySelector(".nav-toggle");
 const links = document.querySelector(".links");
+const navItem = document.querySelector(".nav-item");
 
 navToggle.addEventListener("click", function () {
   links.classList.toggle("show-links");
 });
+
